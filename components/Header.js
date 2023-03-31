fec81c155a472c6e51cfec97a9b02415c4ab5361
Mayer Seidman <mzseidman@gmail.com>
2023-03-31 10:39:41 -0300

    setup

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Header() {
+	return (
+		<Head>
+	    	<title>PROJECT TITLE</title>
+	    	<meta name="PROJECT TITLE" content="Project brief description" />
+	    	<link rel="shortcut icon" href="/circle.png" />
+		</Head>
+	)
+}
