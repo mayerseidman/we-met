af6653daf92c3eea9bae457c2743728dc20d1a50
Mayer Seidman <mzseidman@gmail.com>
2026-02-23 00:04:52 -0500

    fixes quote bug

--- components/ShowQR.js
+++ components/ShowQR.js
@@ -135,8 +135,7 @@ const DesktopEventSelect = ({ selectedEvent, onEventChange }) => (
 const InfoNote = () => (
     <p className={styles.infoNote}>
         <span className={styles.infoIcon}>ⓘ</span>
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        They don't have it?
+        They don&apos;t have it?{" "}
 
         <a href="#" className={styles.infoLink}>
             What to Do
