7bdfbddf87887fdef1dfac3b19e638268d9503dd
Mayer Seidman <mzseidman@gmail.com>
2026-04-02 10:36:03 +0200

    updates next config no more github pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 const nextConfig = {
     reactStrictMode: false,
     swcMinify: true,
-    output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
     images: {
         unoptimized: true,
     },
-    basePath: process.env.NODE_ENV === 'production' ? '/we-met' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/we-met' : '',
 }
 module.exports = nextConfig
